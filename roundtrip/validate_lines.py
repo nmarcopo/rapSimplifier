b7ba15df25b701b6f34909fad17f8d1ced4d105f
Nick Marcopoli <nick@marcopo.li>
2020-04-20 13:41:27 -0400

    validate number of lines present in both files

---
+++ roundtrip/validate_lines.py
@@ -0,0 +1,45 @@
+from os import walk
+from sys import stderr
+from mmap import mmap
+import argparse
+
+def get_file_lines(file_path):
+    with open(file_path, 'r+') as f:
+        buf = mmap(f.fileno(), 0)
+        lines = 0
+        readline = buf.readline
+        # while True:
+        #     x = readline()
+        #     if len(x) == 0:
+        #         break
+        #     lines += 1
+        #     print(x, lines)
+        while readline():
+            lines += 1
+        return lines
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true')
+    debug = parser.parse_args().debug
+
+    lyrics_parent_dir = './'
+    lyrics_backtranslated_dir = 'lyrics_backtranslated/'
+    lyrics_dir = 'lyrics_test/'
+
+    try:
+        (_, _, file_names) = next(walk(lyrics_parent_dir + lyrics_backtranslated_dir))
+    except StopIteration:
+        print(f"Error, make sure directory {lyrics_parent_dir + lyrics_backtranslated_dir} exists.", file=stderr)
+        exit(1)
+
+    file_names_and_lines = {fname : get_file_lines(lyrics_parent_dir + lyrics_backtranslated_dir + fname) for fname in file_names}
+
+    file_names = [fname.replace('.backtranslated', '') for fname in file_names]
+    for fname in file_names:
+        n_lines = get_file_lines(lyrics_parent_dir + lyrics_dir + fname)
+        if file_names_and_lines[fname + '.backtranslated'] != n_lines:
+            print(f"Error, {fname} + backtranslation have different line counts: {file_names_and_lines[fname + '.backtranslated']}, {n_lines}", file=stderr)
+        else:
+            if debug:
+                print(f"No problems here, {file_names_and_lines[fname + '.backtranslated']}, {n_lines}")
