828d55f5c79a66a061f1a0f91cb792abc838892d
Nick Marcopoli <nick@marcopo.li>
2020-04-20 03:33:48 -0400

    added skip for when files are already translated

--- roundtrip/roundtrip_folder.py
+++ roundtrip/roundtrip_folder.py
@@ -8,11 +8,12 @@ def send_output_to_file(file_name):
     global translator
     global lyrics_dir
     global lyrics_parent_dir
+    global lyrics_backtranslated_dir
     # get translation
     result = translator.back_translate_file(file_name)
 
     # send the translated results to a new directory
-    file_name = file_name.replace(lyrics_dir, 'lyrics_backtranslated/') + '.backtranslated'
+    file_name = file_name.replace(lyrics_dir, lyrics_backtranslated_dir) + '.backtranslated'
     result = [r + '\n' for r in result]
     with open(file_name, 'w') as f:
         f.writelines(result)
@@ -26,14 +27,24 @@ if __name__ == '__main__':
     lyrics_dir = 'english/'
     global lyrics_parent_dir
     lyrics_parent_dir = '../data/'
+    global lyrics_backtranslated_dir
+    lyrics_backtranslated_dir = 'lyrics_backtranslated/'
     try:
         (_, _, file_names) = next(walk(lyrics_parent_dir + lyrics_dir))
     except StopIteration:
         print(f"Error, make sure directory {lyrics_parent_dir + lyrics_dir} exists.", file=stderr)
         exit(1)
 
-    # add the directory to file names
-    file_names = [lyrics_parent_dir + lyrics_dir + f for f in file_names]
+    # add the directory to file names if they haven't already been translated
+    try:
+        (_, _, backtranslated_file_names) = next(walk(lyrics_parent_dir + lyrics_backtranslated_dir))
+    except StopIteration:
+        print(f"Error, make sure directory {lyrics_parent_dir + lyrics_backtranslated_dir} exists.", file=stderr)
+        exit(1)
+    file_names = [lyrics_parent_dir + lyrics_dir + f for f in file_names if f + '.backtranslated' not in backtranslated_file_names]
+    if len(file_names) == 0:
+        print("No files to translate. Are all of your files already translated?", file=stderr)
+        exit(1)
     tasks = []
 
     # multiprocessing for getting translations faster
