5e076da38009b29d031e79e9bb5bd54aedc8cfd9
Nick Marcopoli <nick@marcopo.li>
2020-04-20 02:15:15 -0400

    added progress bar

--- roundtrip/roundtrip_folder.py
+++ roundtrip/roundtrip_folder.py
@@ -1,10 +1,13 @@
 from translate import RoundTrip
 from os import walk
 from multiprocessing import Pool
+from tqdm import tqdm
+from sys import stderr, exit
 
 def send_output_to_file(file_name):
     global translator
     global lyrics_dir
+    global lyrics_parent_dir
     # get translation
     result = translator.back_translate_file(file_name)
 
@@ -20,13 +23,20 @@ if __name__ == '__main__':
     translator = RoundTrip()
     num_processes = 10
     global lyrics_dir
-    lyrics_dir = 'lyrics_test/'
-    (_, _, file_names) = next(walk(lyrics_dir))
+    lyrics_dir = 'english/'
+    global lyrics_parent_dir
+    lyrics_parent_dir = '../data/'
+    try:
+        (_, _, file_names) = next(walk(lyrics_parent_dir + lyrics_dir))
+    except StopIteration:
+        print(f"Error, make sure directory {lyrics_parent_dir + lyrics_dir} exists.", file=stderr)
+        exit(1)
+
     # add the directory to file names
-    file_names = [lyrics_dir + f for f in file_names]
+    file_names = [lyrics_parent_dir + lyrics_dir + f for f in file_names]
     tasks = []
 
     # multiprocessing for getting translations faster
     p = Pool(num_processes)
-    for response in p.imap_unordered(send_output_to_file, file_names):
+    for response in tqdm(p.imap_unordered(send_output_to_file, file_names), total=len(file_names)):
         print(response)
